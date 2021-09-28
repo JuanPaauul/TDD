d3c147fc342ab4edaf930a8852200eaa858be7f3
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-09-27 22:34:52 -0400

    Se corrigio un error al momento de crear una variable, el test funciona

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 function calcularTotal(quantity, price, state) {
-  subTotal = quantity * price;
+  var subTotal = quantity * price;
   var tax;
     switch(state){
-        case "UT":
-            
-            break;
-        case "NV":
-            
-            break;
-        case "TX":
-            
-            break;
-        case "AL":
-            
-            break;
-        case "CA":
-            
-            break;
-        default:
-            
-            break;
+      case "UT":
+          tax = 0.0665;
+          break;
+      case "NV":
+          tax = 0.08;
+          break;
+      case "TX":
+          tax = 0.0625;
+          break;
+      case "AL":
+          tax = 0.04;
+          break;
+      case "CA":
+          tax = 0.0825;
+          break;
+      default:
+          
+          break;
     }
   return subTotal+(subTotal*tax);
 }
